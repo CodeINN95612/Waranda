5767674e8af67e16f16b1b803ce29cd7d7242b40
CodeINN95612 <insertnickname1@gmail.com>
2022-06-18 11:45:46 -0500

    Lanzar huevos
    Lanzar huevos

--- Assets/Scripts/Items/Throwable.cs
+++ Assets/Scripts/Items/Throwable.cs
@@ -1,7 +1,26 @@
+using System;
 using UnityEngine;
 
 public class Throwable : MonoBehaviour, IInteractableData
 {
-    public int count;
-    public Sprite sprite;
+  public int count;
+  public Sprite sprite;
+  public GameObject obj;
+  public float upForce = 2.0f;
+  public float frontForce = 3.0f;
+
+  public void Throw(Vector3 initialPos, Vector3 forward)
+  {
+    if (count == 0)
+      return;
+
+    count--;
+
+    GameObject newObj = Instantiate(obj, initialPos, Quaternion.identity);
+    newObj.GetComponent<Rigidbody>().AddForce(Vector3.up * upForce, ForceMode.Impulse);
+    newObj.GetComponent<Rigidbody>().AddForce(forward * frontForce, ForceMode.Impulse);
+
+    Debug.Log(upForce);
+    Debug.Log(frontForce);
+  }
 }
--- Assets/Scripts/Player/PlayerInventoryController.cs
+++ Assets/Scripts/Player/PlayerInventoryController.cs
@@ -5,128 +5,130 @@ using UnityEngine;
 
 public class PlayerInventoryController : MonoBehaviour
 {
-    public GameObject primaryWeaponPos;
-    public GameObject weaponParent;
-    public List<Weapon> weapons = new List<Weapon>(); //Maximo 2
-    public Throwable throwable = null;
+  public GameObject primaryWeaponPos;
+  public GameObject weaponParent;
+  public List<Weapon> weapons = new List<Weapon>(); //Maximo 2
+  public Throwable throwable = null;
 
-    private int _selectedWeaponIndex = -1;
+  public GameObject throwableInitialPos;
 
-    private bool _update = true;
+  private int _selectedWeaponIndex = -1;
 
-    private int primaryIndex => 0;
-    private int secondaryIndex => 1;
+  private bool _update = true;
 
-    public void ChangePrimary(Weapon weapon)
+  private int primaryIndex => 0;
+  private int secondaryIndex => 1;
+
+  public void ChangePrimary(Weapon weapon)
+  {
+    ChangeWeapon(weapon, primaryIndex);
+  }
+
+  public void ChangeSecondary(Weapon weapon)
+  {
+    ChangeWeapon(weapon, secondaryIndex);
+  }
+
+  private void ChangeWeapon(Weapon weapon, int index)
+  {
+    InitWeapons();
+    weapons[index] = weapon;
+    if (_selectedWeaponIndex == -1)
     {
-        ChangeWeapon(weapon, primaryIndex);
+      _selectedWeaponIndex = index;
+    }
+    _update = true;
+  }
+
+  public void ChangeThrowable(Throwable th)
+  {
+    throwable = th;
+    _update = true;
+  }
+
+  public void InitWeapons()
+  {
+    while (weapons.Count < 2)
+    {
+      weapons.Add(null);
+    }
+  }
+
+  public void ShowCurrent()
+  {
+    if (_selectedWeaponIndex == -1 || !_update)
+    {
+      return;
+    }
+    _update = false;
+
+    foreach (Transform child in weaponParent.transform)
+    {
+      GameObject.Destroy(child.gameObject);
     }
 
-    public void ChangeSecondary(Weapon weapon)
+    GameObject obj = GameObject.Instantiate(weapons[_selectedWeaponIndex].prefab, primaryWeaponPos.transform);
+    obj.transform.position = primaryWeaponPos.transform.position;
+    obj.transform.parent = weaponParent.transform;
+    Destroy(obj.GetComponent<BoxCollider>());
+    obj.SetActive(true);
+  }
+
+  public Weapon GetCurrent()
+  {
+    if (_selectedWeaponIndex == -1)
     {
-        ChangeWeapon(weapon, secondaryIndex);
+      return null;
     }
 
-    private void ChangeWeapon(Weapon weapon, int index)
+    return weapons[_selectedWeaponIndex];
+  }
+
+  public bool Updated()
+  {
+    return _update;
+  }
+
+  public void RechargePrimary(float val)
+  {
+    if (_selectedWeaponIndex != primaryIndex)
     {
-        InitWeapons();
-        weapons[index] = weapon;
-        if (_selectedWeaponIndex == -1)
-        {
-            _selectedWeaponIndex = index;
-        }
-        _update = true;
+      return;
     }
 
-    public void ChangeThrowable(Throwable th)
+    Weapon wp = weapons[_selectedWeaponIndex];
+    wp.percentage = MathF.Min(1.0f, wp.percentage + val);
+    _update = true;
+  }
+
+  public void Shoot(float val)
+  {
+    if (_selectedWeaponIndex == -1)
     {
-        throwable = th;
-        _update = true;
+      return;
     }
 
-    public void InitWeapons()
+    Weapon wp = weapons[_selectedWeaponIndex];
+    if (_selectedWeaponIndex == primaryIndex)
     {
-        while (weapons.Count < 2)
-        {
-            weapons.Add(null);
-        }
+      wp.percentage = MathF.Max(0.0f, wp.percentage - val);
+      _update = true;
+    }
+  }
+
+  public Throwable GetThrowable()
+  {
+    return throwable;
+  }
+
+  public void Throw(int count)
+  {
+    if (throwable == null)
+    {
+      return;
     }
 
-    public void ShowCurrent()
-    {
-        if (_selectedWeaponIndex == -1 || !_update)
-        {
-            return;
-        }
-        _update = false;
-
-        foreach (Transform child in weaponParent.transform)
-        {
-            GameObject.Destroy(child.gameObject);
-        }
-
-        GameObject obj = GameObject.Instantiate(weapons[_selectedWeaponIndex].prefab, primaryWeaponPos.transform);
-        obj.transform.position = primaryWeaponPos.transform.position;
-        obj.transform.parent = weaponParent.transform;
-        Destroy(obj.GetComponent<BoxCollider>());
-        obj.SetActive(true);
-    }
-
-    public Weapon GetCurrent()
-    {
-        if (_selectedWeaponIndex == -1)
-        {
-            return null;
-        }
-
-        return weapons[_selectedWeaponIndex];
-    }
-
-    public bool Updated()
-    {
-        return _update;
-    }
-
-    public void RechargePrimary(float val)
-    {
-        if (_selectedWeaponIndex != primaryIndex)
-        {
-            return;
-        }
-
-        Weapon wp = weapons[_selectedWeaponIndex];
-        wp.percentage = MathF.Min(1.0f, wp.percentage + val);
-        _update = true;
-    }
-
-    public void Shoot(float val)
-    {
-        if (_selectedWeaponIndex == -1)
-        {
-            return;
-        }
-
-        Weapon wp = weapons[_selectedWeaponIndex];
-        if (_selectedWeaponIndex == primaryIndex)
-        {
-            wp.percentage = MathF.Max(0.0f, wp.percentage - val);
-            _update = true;
-        }
-    }
-
-    public Throwable GetThrowable()
-    {
-        return throwable;
-    }
-
-    public void Throw(int count)
-    {
-        if (throwable == null)
-        {
-            return;
-        }
-
-        throwable.count = Math.Max(0, throwable.count - 1);
-        _update = true;
-    }
+    throwable.Throw(throwableInitialPos.transform.position, transform.TransformDirection(Vector3.forward));
+    _update = true;
+  }
 }
